76b1204169c5603b61b810cc5c2d21b4f1c94454
whyisyoung <liminy2@illinois.edu>
2020-10-23 21:53:56 -0700

    add new paper records USENIX Security 2021 and summary of each paper, CV is also updated

--- archive/index.php
+++ archive/index.php
@@ -2,7 +2,7 @@
  * @Author: Jon Barron
  * @Date: 2019-05-11 22:15:22
  * @LastEditors: whyisyoung
- * @LastEditTime: 2020-06-28 15:06:46
+ * @LastEditTime: 2020-10-23 10:57:25
  -->
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
@@ -60,7 +60,7 @@
     papertitle {
       font-family: 'Lato', Verdana, Helvetica, sans-serif;
       font-size: 16px;
-      /* font-weight: 700 */
+      font-weight: bold;
     }
 
     name {
@@ -100,10 +100,23 @@
       background-color: #ffffd0;
     }
 
+    span.artifact {
+      color:#6cb41b;
+      padding: 1px;
+    }
+
+    span.underline {
+      border-bottom:1px solid black;
+      padding-bottom:1px;
+    }
+
+    span.tldr {
+      color: #555555;
+    }
+
     em.highlight {
       color: #e37222;
     }
-
   </style>
   <link rel="icon" type="image/png" href="images/limin-icon.jpg">
   <title>Limin Yang</title>
@@ -112,7 +125,7 @@
 </head>
 
 <body>
-  <table width="860" border="0" align="center" cellspacing="0" cellpadding="0">
+  <table width="960" border="0" align="center" cellspacing="0" cellpadding="0">
     <tr>
       <td>
         <!-- Intro Begin -->
@@ -124,14 +137,14 @@
           <tr>
             <td valign="middle">
               <img src="images/liminyang.jpg" height="170px">
-              <p class="footer" style="font-size: 15px;">photo credit: <a href="https://www.haileyliu.photos/" target="_blank" style="font-size: 15px"> Hailey Liu</a>  </p>
+              <p class="footer" style="font-size: 15px;">Photo credit: <a href="https://www.haileyliu.photos/" target="_blank" style="font-size: 15px"> Hailey Liu</a>  </p>
             </td>
             <td valign="middle">
               <p align="left">
                 <name>Limin Yang &nbsp; (杨利民) </name>
               </p>
               <p>I am a Ph.D. student of Computer Science at <a href="https://illinois.edu">UIUC</a> since 08/2019. <br>
-                 I focus on <em class="highlight">security, measurement, and explainable AI. </em> <br>
+                 I focus on <em class="highlight">machine learning security, measurement, and explainable AI. </em> <br>
                  My advisor is Dr. <a href="https://gangw.web.illinois.edu/">Gang Wang</a>.
               </p>
 
@@ -164,11 +177,21 @@
           </colgroup>
           <tr>
             <td valign="center">
+              09/30/2020
+            </td>
+            <td valign="center">
+                Our USENIX Security'21 paper got accepted! <br>
+                Thanks for everyone who helped me!
+            </td>
+          </tr>
+          <tr>
+            <td valign="center">
               08/01/2020
             </td>
             <td valign="center" style="color:#E9623F">
                   <strong>I'm actively looking for a security and deep learning internship for 2021 summer!</strong>
             </td>
+          </tr>
           <tr>
             <td valign="center">
               06/18/2020
@@ -197,18 +220,38 @@
           </colgroup>
           <tr>
             <td valign="center">
-                <strong>[USENIX Security'20]</strong>
+                <papertitle>[USENIX Security'21]</papertitle>
+            </td>
+            <td valign="center">
+                <papertitle>CADE: Detecting and Explaining Concept Drift Samples for Security Applications</papertitle>
+                <br>
+                <span class="underline">Limin Yang</span>,
+                Wenbo Guo, Qingying Hao, Arridhana Ciptadi, Ali Ahmadzadeh, Xinyu Xing, Gang Wang
+                <br>
+                <a href="https://github.com/whyisyoung/CADE">Code</a> /
+                <span class="artifact">Artifact Evaluated</span>
+                <br>
+                <span class="tldr">TL;DR:  Contrastive learning can help detect drifting samples from previously unseen classes and distance-based explanation can find out why such drifting samples are different from existing classes. </span>
+            </td>
+          </tr>
+          <tr>
+            <td valign="center">
+                <papertitle>[USENIX Security'20]</papertitle>
             </td>
             <td valign="center">
                 <papertitle>Measuring and Modeling the Label Dynamics of Online Anti-Malware Engines</papertitle>
                 <br>
                 Shuofei Zhu,
                 Jianjun Shi,
-                <strong><u>Limin Yang</u></strong>,
+                <span class="underline">Limin Yang</span>,
                 Boqin Qin, Ziyi Zhang, Linhai Song, Gang Wang
                 <br>
                 <a href="data/USENIX20_VirusTotal.pdf">PDF</a> /
-                <a href="https://sfzhu93.github.io/projects/vt/index.html">Data</a>
+                <a href="https://sfzhu93.github.io/projects/vt/index.html">Data</a> /
+                <!-- <a href="https://sfzhu93.github.io/projects/vt/paper-list.html">Papers used VirusTotal</a> / -->
+                <span class="artifact">Artifact Evaluated</span>
+                <br>
+                <span class="tldr">TL;DR:  How to aggregate VirusTotal labels more properly? </span>
             </td>
           </tr>
           <tr>
@@ -219,39 +262,43 @@
                 <papertitle>A Case Study of the Security Vetting Process of Smart-home Assistant Applications</papertitle>
                 <br>
                 Hang Hu,
-                <strong><u>Limin Yang</u></strong>,
+                <span class="underline">Limin Yang</span>,
                 Shihan Lin,
                 Gang Wang
                 <br>
                 <a href="data/SafeThings20_Alexa.pdf">PDF</a>
+                <br>
+                <span class="tldr">TL;DR: Verify that replay attack and SQL injection are feasible in Amazon Alexa / Google Home apps. </span>
             </td>
           </tr>
           <tr>
             <td valign="center">
-                <strong>[IMC'19]</strong>
+                <papertitle>[IMC'19]</papertitle>
             </td>
             <td valign="center">
                 <papertitle>Opening the Blackbox of VirusTotal: Analyzing Online Phishing Scan Engines</papertitle>
                 <br>
                 Peng Peng,
-                <strong><u>Limin Yang</u></strong>,
+                <span class="underline">Limin Yang</span>,
                 Linhai Song,
                 Gang Wang
                 <br>
                 <a href="data/IMC19_VirusTotal.pdf">PDF</a> /
                 <a href="https://github.com/whyisyoung/virustotal">Data</a>
+                <br>
+                <span class="tldr">TL;DR: Control simple phishing sites to see how bad and inconsistent of the URL detection provided by VirusTotal and security vendors' own APIs. </span>
             </td>
           </tr>
           <tr>
             <td valign="center">
-                <strong>[USENIX Security'18]</strong>
+                <papertitle>[USENIX Security'18]</papertitle>
             </td>
             <td valign="center">
                 <papertitle>Understanding the Reproducibility of Crowd-reported Security Vulnerabilities</papertitle>
                 <br>
                 Dongliang Mu,
                 Alejandro Cuevas,
-                <strong><u>Limin Yang</u></strong>,
+                <span class="underline">Limin Yang</span>,
                 Hang Hu,
                 Xinyu Xing,
                 Bing Mao,
@@ -259,6 +306,9 @@
                 <br>
                 <a href="data/USENIX18_VulReproduction.pdf">PDF</a> /
                 <a href="https://vulnreproduction.github.io/">Data</a>
+                <br>
+                <span class="tldr">TL;DR: Provide quantitive evidence on the
+                  prevalence of missing information in vulnerability reports and low reproducibility by manually reproduce 368 memory corruption bugs. </span>
             </td>
           </tr>
           <tr>
@@ -268,11 +318,13 @@
             <td valign="center">
                 <papertitle>VulDigger: A Just-in-Time and Cost-Aware Tool for Digging Vulnerability-Contributing Changes</papertitle>
                 <br>
-                <strong><u>Limin Yang</u></strong>,
+                <span class="underline">Limin Yang</span>,
                 <!-- Xiangxue Li, Yu Yu -->
                 et al.
                 <br>
                 <a href="data/Globecom17_Vuldigger.pdf">PDF</a>
+                <br>
+                <span class="tldr">TL;DR: Use metadata and heuristics to predict whether a git commit introduced a vulnerability. </span>
             </td>
           </tr>
           <tr>
@@ -283,11 +335,13 @@
                 <papertitle>Characterizing User Behaviors in Location-based Find-and-Flirt Services: Anonymity and Demographics</papertitle>
                 <br>
                 Minhui Xue,
-                <strong><u>Limin Yang</u></strong>,
+                <span class="underline">Limin Yang</span>,
                 Keith W. Ross,
                 Haifeng Qian
                 <br>
                 <a href="data/PPNA17_WeChat.pdf">PDF</a>
+                <br>
+                <span class="tldr">TL;DR: Who used the WeChat "People Nearby" feature more often? </span>
             </td>
           </tr>
         </table>
@@ -319,7 +373,7 @@
           </colgroup>
           <tr>
             <td valign="center">
-              <a href="https://www.ist.psu.edu/"><papertitle>Penn State</papertitle></a>
+              <a href="https://www.ist.psu.edu/"><span>Penn State</span></a>
             </td>
             <td valign="center">
                 <em>Research Intern</em>, Pennsylvania, US, 09/2017 – 02/2018
@@ -327,7 +381,7 @@
           </tr>
           <tr>
             <td valign="center">
-                <a href="https://www.xueba100.com/"><papertitle>Xueba Jun</papertitle></a>
+                <a href="https://www.xueba100.com/"><span>Xueba Jun</span></a>
             </td>
             <td valign="center">
               <em>Search and Rank Intern</em>, Shanghai, China, 09/2016 – 10/2016
@@ -335,8 +389,8 @@
           </tr>
           <tr>
             <td valign="center">
-                <a href="http://english.pku.edu.cn/"><papertitle>Peking University</
-                  papertitle></a>
+                <a href="http://english.pku.edu.cn/"><span>Peking University</
+                  span></a>
             </td>
             <td valign="center">
               <em>Exploit Intern</em>, Beijing, China, 07/2015 – 08/2015
@@ -363,7 +417,7 @@
               2017
             </td>
             <td>
-                <papertitle> ECNU Graduate Student Overseas Research Scholarship</papertitle>
+                <span> ECNU Graduate Student Overseas Research Scholarship</span>
             </td>
           </tr>
           <tr>
@@ -371,7 +425,7 @@
               2013-2015
             </td>
             <td>
-                <papertitle>ECNU Top-notch Innovative Personnel Training Plan</papertitle>
+                <span>ECNU Top-notch Innovative Personnel Training Plan</span>
             </td>
           </tr>
         </table>
@@ -395,7 +449,7 @@
                 2019 Spring
             </td>
             <td>
-                <papertitle>CS-4264 Principles of Computer Security</papertitle>,
+                <span>CS-4264 Principles of Computer Security</span>,
                 Virginia Tech, Teaching Assistant
             </td>
           </tr>
@@ -404,12 +458,27 @@
               2018 Fall
             </td>
             <td>
-                <papertitle>CS-3114 Data Structures and Algorithms</papertitle>,
+                <span>CS-3114 Data Structures and Algorithms</span>,
                 Virginia Tech, Teaching Assistant
             </td>
           </tr>
         </table>
         <!-- Teaching End -->
+        <br>
+        <table width="100%" align="center" border="0" cellpadding="12" style="table-layout:fixed; color: #888;">
+          <colgroup>
+                <col span="1" style="width: 23%;">
+                <col span="1" style="width: 77%;">
+          </colgroup>
+          <tr>
+            <td valign="center">
+              Miscellaneous
+            </td>
+            <td>
+                I'm also a minimalism photographer: <a href="http://whyisyoung.com" target="_blank">whyisyoung.com</a>
+            </td>
+          </tr>
+        </table>
 
         <table width="100%" align="center" border="0" cellpadding="12">
           <tr>
